c41e46737978e4d992320ea2f2a36acfcb6129ef
srodrigues-adnovum <89187311+srodrigues-adnovum@users.noreply.github.com>
2024-05-09 09:29:58 +0100

    rubygems reshim issue

--- rubygems-plugin/rubygems_plugin.rb
+++ rubygems-plugin/rubygems_plugin.rb
@@ -2,7 +2,7 @@ module ReshimInstaller
   def install(options)
     super
     # We don't know which gems were installed, so always reshim.
-    `asdf reshim ruby`
+    `bash -c 'asdf reshim ruby'`
   end
 end
 
@@ -12,12 +12,12 @@ else
   Gem.post_install do |installer|
     # Reshim any (potentially) new executables.
     installer.spec.executables.each do |executable|
-      `asdf reshim ruby #{RUBY_VERSION} bin/#{executable}`
+      `bash -c 'asdf reshim ruby #{RUBY_VERSION} bin/#{executable}'`
     end
   end
   Gem.post_uninstall do |installer|
     # Unfortunately, reshimming just the removed executables or
     # ruby version doesn't work as of 2020/04/23.
-    `asdf reshim ruby` if installer.spec.executables.any?
+    `bash -c 'asdf reshim ruby'` if installer.spec.executables.any?
   end
 end
